7210036743d75ce7112c23f5f0e6a54f1a7fb180
Ivan Chau <ivanmchau@gmail.com>
2017-07-17 18:21:56 -0400

    table population

--- client/src/components/profile/Table.js
+++ client/src/components/profile/Table.js
@@ -1,26 +1,47 @@
 import React, { Component, PropTypes } from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-
+import './react-table.css'
 export default class Table extends Component{
 	add(){
     	console.log('add clicked')
   	}
-	render(){
-		const {} = this.props
-		return (
-			<div className='runs'>
-				<h1>helo</h1>
-				<div id="addRun" className='mdc-fab' onClick={this.add.bind(this)}>
-					<h3 id="add"> + </h3>
-				</div>
-			</div>
-		);
-	}
+  	render(){
+  		const {} = this.props
+  		const options = {
+  			onRowClick: function(row) {
+    			alert(`You click row id: ${row.id}`);
+  			},
+  			onRowDoubleClick: function(row) {
+    			alert(`You double click row id: ${row.id}`);
+  			}
+		};
+  		var products = [{
+  			length: "3.3 miles",
+  			time: "15m 23s",
+  			pace: "5:12"
+  		},{
+  			length: "0.9 miles",
+  			time: "4m 23s",
+  			pace: "3:12"
+  		}];
+		// It's a data format example.
+  		return (
+  			<div className='runs'>
+  				<BootstrapTable data={products} options={options} striped={false} hover={true} bordered={false} tableStyle={ { "padding": "6px" } }>
+  					<TableHeaderColumn dataField="length" width='60%' isKey={true} dataAlign="left" dataSort={true}>Length</TableHeaderColumn>
+  					<TableHeaderColumn dataField="time" width='20%' dataAlign="center" dataSort={true}>Time</TableHeaderColumn>
+  					<TableHeaderColumn dataField="pace" width='20%' dataAlign="center">Pace</TableHeaderColumn>
+  				</BootstrapTable>
+  				<div id="addRun" className='mdc-fab' onClick={this.add.bind(this)}>
+  					<h3 id="add"> + </h3>
+  				</div>
+  			</div>
+  		);
+  	}
 }
-/*
+
 Table.propTypes = {
 	data : PropTypes.arrayOf(
 		PropTypes.object.isRequired
 	).isRequired,
 }
-*/
