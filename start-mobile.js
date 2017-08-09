75a4ed58b681fd422b4a9202428e679d373f969c
Ivan Chau <ivanmchau@gmail.com>
2017-08-08 21:14:38 -0400

    clean

--- start-mobile.js
+++ start-mobile.js
@@ -1,4 +1,3 @@
-console.log('log HELLO + ++ +')
 const args = [ 'run ios' ];
 const opts = { stdio: 'inherit', cwd: 'client/mobile', shell: true };
 require('child_process').spawn('npm', args, opts);
